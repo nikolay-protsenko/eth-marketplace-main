d863fdb8f8ca36c0b2eef444142bc5122d244f95
nikolay <nisan89.prots@gmail.com>
2023-07-21 15:17:33 +0600

    fix css

--- components/ui/common/hero/index.js
+++ components/ui/common/hero/index.js
@@ -10,9 +10,12 @@ export default function Hero() {
       <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">Discover everything you need for pleasure, convenience, and inspiration. We bring together thousands of sellers and buyers from all around the world to create the most diverse and exciting shopping destination.</div>
       <div className="mt-5 sm:mt-8 flex lg:justify-start">
         <div className="rounded-md shadow">
-          <Link href="/marketplace" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
+          <div className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
+          <Link href="/marketplace" >
             Get started
           </Link>
+          </div>
+          
         </div>
       </div>
     </section>
